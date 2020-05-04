c544f904a8c1e475d41ee30e817688e85bb8354a
veikai@126.com <veikai@126.com>
2020-05-04 15:22:59 +0800

    代码提交

--- GUI.py
+++ GUI.py
@@ -1,12 +1,21 @@
 import tkinter
 from tkinter import ttk
-import time
+from PyWeChatSpy import WeChatSpy
+from time import sleep
+import pickle
+import re
+import os
+
+data_global = {}
+listen_index = 1
+if os.path.exists("data.pkl"):
+    with open("data.pkl", "rb") as rf:
+        data_global = pickle.load(rf)
 
 
 class Postman:
     def __init__(self):
         self.tk = tkinter.Tk()
-        # self.tk.overrideredirect(True)
         self.tk.title("鲁班转发助手")
         self.tk.geometry('600x400+200+200')
         self.tk.resizable(0, 0)
@@ -14,21 +23,26 @@ class Postman:
         self.label_nickname.pack()
         self.frame_group = tkinter.LabelFrame(self.tk, text="转发关系", padx=5, pady=5)
         self.frame_group.place(x=10, y=20)
-        self.group_forward = ttk.Treeview(self.frame_group, show="headings", columns=('col1', 'col2', 'col3', 'col4'))
+        self.group_forward = ttk.Treeview(self.frame_group, show="headings", columns=('col0', 'col1', 'col2', 'col3', 'col4'))
+        self.group_forward.heading('col0', text='ID')
         self.group_forward.heading('col1', text='监听群')
         self.group_forward.heading('col2', text='监听详情')
         self.group_forward.heading('col3', text='转发群')
         self.group_forward.heading('col4', text='回复详情')
+        self.group_forward.column('col0', width=20, anchor='center')
         self.group_forward.column('col1', width=120, anchor='center')
         self.group_forward.column('col2', width=120, anchor='center')
         self.group_forward.column('col3', width=120, anchor='center')
         self.group_forward.column('col4', width=120, anchor='center')
-        # self.group_forward.bind("<ButtonRelease-1>", self.treeviewClick)
         self.group_forward.pack()
+        self.label_log = tkinter.Label(self.tk, text="日志通知")
+        self.label_log.place(x=10, y=300)
+        self.button_open_wechat = tkinter.Button(self.tk, text="打开微信", command=self.open_wechat)
+        self.button_open_wechat.place(x=535, y=30)
         self.button_add_listen = tkinter.Button(self.tk, text="添加监听", command=self.select_group_listen)
-        self.button_add_listen.place(x=520, y=30)
-        self.button_add_listen = tkinter.Button(self.tk, text="删除监听", command=self.select_group_listen)
-        self.button_add_listen.place(x=520, y=70)
+        self.button_add_listen.place(x=535, y=70)
+        self.button_add_listen = tkinter.Button(self.tk, text="删除监听", command=self.delete_listen_relationship)
+        self.button_add_listen.place(x=535, y=110)
         self.group_members = []
         self.combobox_group_listen = None
         self.combobox_group_reply = None
@@ -43,15 +57,81 @@ class Postman:
         self.group_reply = None
         self.members_listen = []
         self.members_reply = []
+        self.pid = 0
+        self.spy = WeChatSpy(parser=self.parser, multi=True)
+        self.wxid = None
+
+    def parser(self, data):
+        global listen_index
+        m_type = data.pop("type")
+        pid = data.pop("pid")
+        if m_type == 1:
+            self.wxid = data["wxid"]
+            if not data_global.get(self.wxid):
+                data_global[data["wxid"]] = {
+                    "listen_relationship": {},
+                    "pid_chatroom": {},
+                    "chatroom_nickname": {},
+                    "chatroom_member": {},
+                    "member_nickname": {}
+                }
+            for k, v in data_global[self.wxid]["listen_relationship"].items():
+                self.group_forward.insert('', 'end', values=(
+                    k, data_global[self.wxid]["chatroom_nickname"][v["group_listen"]], '11',
+                    data_global[self.wxid]["chatroom_nickname"][v["group_reply"]], '22'))
+            id_list = list(data_global[self.wxid]["listen_relationship"].keys())
+            if id_list:
+                listen_index += id_list[-1]
+            self.spy.query_contact_list(step=50, pid=pid)
+        elif m_type == 3:
+            for contact in data["data"]:
+                if contact["wxid"].endswith("@chatroom"):
+                    data_global[self.wxid]["chatroom_nickname"][contact["wxid"]] = contact["nickname"]
+                    if not data_global[self.wxid]["pid_chatroom"].get(pid):
+                        data_global[self.wxid]["pid_chatroom"][pid] = [contact]
+                    else:
+                        data_global[self.wxid]["pid_chatroom"][pid].append(contact)
+            if data["total_page"] == data["current_page"]:
+                self.label_log["text"] = "联系人加载完成"
+        elif m_type == 4:
+            data_global[self.wxid]["chatroom_member"][data["wxid"]] = data["data"]
+            for member in data["data"]:
+                data_global[self.wxid]["member_nickname"][member["wxid"]] = member["nickname"]
+        elif m_type == 5:
+            if pid == self.pid:
+                for message in data["data"]:
+                    if message["msg_type"] == 1:
+                        wxid1 = message["wxid1"]
+                        wxid2 = message.get("wxid2")
+                        content = message["content"]
+                        if wxid2:
+                            for listen in data_global[self.wxid]["listen_relationship"].values():
+                                if wxid1 == listen["group_listen"] and wxid2 in listen["member_listen"]:
+                                    wxid_forward = listen["group_reply"]
+                                    group_nickname = data_global[self.wxid]["chatroom_nickname"][wxid1]
+                                    speaker_nickname = data_global[self.wxid]["member_nickname"][wxid2]
+                                    content = f"鲁班转发：[{group_nickname}]{speaker_nickname}：{content}"
+                                    self.spy.send_text(wxid_forward, content, pid=self.pid)
+                                elif wxid1 == listen["group_reply"] and wxid2 in listen["member_reply"]:
+                                    wxid_forward = listen["group_listen"]
+                                    group_nickname = data_global[self.wxid]["chatroom_nickname"][wxid1]
+                                    speaker_nickname = data_global[self.wxid]["member_nickname"][wxid2]
+                                    content = f"鲁班转发：[{group_nickname}]{speaker_nickname}：{content}"
+                                    self.spy.send_text(wxid_forward, content, pid=self.pid)
+
+    def open_wechat(self):
+        self.pid = self.spy.run(background=True)
 
     def select_group_listen(self):
+        if not self.wxid:
+            self.label_log["text"] = "请先登录微信"
+            return
         self.tl = tkinter.Toplevel(self.tk)
-        # self.tk.overrideredirect(True)
         self.tl.title("选择监听群")
         self.tl.geometry('250x80+300+300')
         self.tl.resizable(0, 0)
         self.combobox_group_listen = ttk.Combobox(self.tl)
-        self.combobox_group_listen['value'] = ('上海', '北京', '天津', '广州')
+        self.combobox_group_listen['value'] = [f"{i['nickname']}[{i['wxid']}]" for i in data_global[self.wxid]["pid_chatroom"][self.pid]]
         self.combobox_group_listen.pack(pady=10)
         button_next = tkinter.Button(self.tl, text="下一步", command=self.select_group_member_listen)
         button_next.pack()
@@ -59,17 +139,22 @@ class Postman:
     def select_group_member_listen(self):
         self.group_listen = self.combobox_group_listen.get()
         self.tl.destroy()
-        print(self.group_listen)
+        self.group_listen = re.search(r'(?<=\[).*?(?=\])', self.group_listen).group()
+        self.spy.query_chatroom_member(self.group_listen, self.pid)
+        while True:
+            if data_global[self.wxid]["chatroom_member"].get(self.group_listen):
+                self.group_members = [f"{i['nickname']}[{i['wxid']}]" for i in data_global[self.wxid]["chatroom_member"][self.group_listen]]
+                break
+            sleep(0.1)
         self.tl = tkinter.Toplevel(self.tk)
         self.tl.title("选择监听群成员")
         self.tl.geometry('200x250+300+300')
         self.tl.resizable(0, 0)
-        self.group_members = [int(time.time()) + i for i in range(500)]
         self.listbox_member_listen = tkinter.Listbox(self.tl, selectmode=tkinter.MULTIPLE)
         self.listbox_member_listen.pack()
         for member in self.group_members:
             self.listbox_member_listen.insert("end", member)
-        self.check_member_listen = tkinter.Checkbutton(self.tl, text="全选1", command=self.select_all_member_listen)
+        self.check_member_listen = ttk.Checkbutton(self.tl, text="全选1", command=self.select_all_member_listen)
         self.check_member_listen.pack()
         button_next = tkinter.Button(self.tl, text="下一步", command=self.select_group_reply)
         button_next.pack()
@@ -84,37 +169,44 @@ class Postman:
 
     def select_group_reply(self):
         self.select_all_listen = False
-        self.members_listen = self.listbox_member_listen.selection_get().split("\n")
+        for i in self.listbox_member_listen.selection_get().split("\n"):
+            self.members_listen.append(re.search(r'(?<=\[).*?(?=\])', i).group())
         self.tl.destroy()
-        print(self.members_listen, len(self.members_listen))
         self.tl = tkinter.Toplevel(self.tk)
-        # self.tk.overrideredirect(True)
         self.tl.title("选择转发群")
         self.tl.geometry('250x80+300+300')
         self.tl.resizable(0, 0)
         self.combobox_group_reply = ttk.Combobox(self.tl)
-        self.combobox_group_reply['value'] = ('上海', '北京', '天津', '广州')
+        self.combobox_group_reply['value'] = [f"{i['nickname']}[{i['wxid']}]" for i in data_global[self.wxid]["pid_chatroom"][self.pid]]
         self.combobox_group_reply.pack(pady=10)
         button_next = tkinter.Button(self.tl, text="下一步", command=self.select_group_member_reply)
         button_next.pack()
 
     def select_group_member_reply(self):
         self.group_reply = self.combobox_group_reply.get()
-        self.tl.destroy()
-        print(self.group_reply)
-        self.tl = tkinter.Toplevel(self.tk)
-        self.tl.title("选择转发回复群成员")
-        self.tl.geometry('200x250+300+300')
-        self.tl.resizable(0, 0)
-        self.group_members = [int(time.time()) + i for i in range(500)]
-        self.listbox_member_reply = tkinter.Listbox(self.tl, selectmode=tkinter.MULTIPLE)
-        self.listbox_member_reply.pack()
-        for member in self.group_members:
-            self.listbox_member_reply.insert("end", member)
-        self.check_member_reply = tkinter.Checkbutton(self.tl, text="全选2", command=self.select_all_member_reply)
-        self.check_member_reply.pack()
-        button_next = tkinter.Button(self.tl, text="确认添加", command=self.add_listen_relationship)
-        button_next.pack()
+        self.group_reply = re.search(r'(?<=\[).*?(?=\])', self.group_reply).group()
+        if self.group_listen == self.group_reply:
+            self.label_log["text"] = "监听群与转发群不允许相同"
+        else:
+            self.tl.destroy()
+            self.spy.query_chatroom_member(self.group_reply, self.pid)
+            while True:
+                if data_global[self.wxid]["chatroom_member"].get(self.group_reply):
+                    self.group_members = [f"{i['nickname']}[{i['wxid']}]" for i in data_global[self.wxid]["chatroom_member"][self.group_reply]]
+                    break
+                sleep(0.1)
+            self.tl = tkinter.Toplevel(self.tk)
+            self.tl.title("选择转发回复群成员")
+            self.tl.geometry('200x250+300+300')
+            self.tl.resizable(0, 0)
+            self.listbox_member_reply = tkinter.Listbox(self.tl, selectmode=tkinter.MULTIPLE)
+            self.listbox_member_reply.pack()
+            for member in self.group_members:
+                self.listbox_member_reply.insert("end", member)
+            self.check_member_reply = ttk.Checkbutton(self.tl, text="回复全选", command=self.select_all_member_reply)
+            self.check_member_reply.pack()
+            button_next = tkinter.Button(self.tl, text="确认添加", command=self.add_listen_relationship)
+            button_next.pack()
 
     def select_all_member_reply(self):
         if not self.select_all_reply:
@@ -125,10 +217,34 @@ class Postman:
             self.listbox_member_reply.selection_clear(0, len(self.group_members))
 
     def add_listen_relationship(self):
+        global listen_index
         self.select_all_reply = False
-        self.members_reply = self.listbox_member_reply.selection_get().split("\n")
+        for i in self.listbox_member_reply.selection_get().split("\n"):
+            self.members_reply.append(re.search(r'(?<=\[).*?(?=\])', i).group())
         self.tl.destroy()
-        print(self.members_reply, len(self.members_reply))
+        listen_relationship = {
+            "group_listen": self.group_listen,
+            "member_listen": self.members_listen,
+            "group_reply": self.group_reply,
+            "member_reply": self.members_reply
+        }
+        data_global[self.wxid]["listen_relationship"][listen_index] = listen_relationship
+        with open("data.pkl", "wb") as wf:
+            pickle.dump(data_global, wf)
+        self.group_forward.insert('', 'end', values=(listen_index, data_global[self.wxid]["chatroom_nickname"][self.group_listen], '11', data_global[self.wxid]["chatroom_nickname"][self.group_reply], '22'))
+        listen_index += 1
+
+    def delete_listen_relationship(self):
+        if not self.wxid:
+            self.label_log["text"] = "请先登录微信"
+            return
+        selected_items = self.group_forward.selection()
+        listen_id = self.group_forward.item(selected_items)["values"][0]
+        for item in selected_items:
+            self.group_forward.delete(item)
+        data_global[self.wxid]["listen_relationship"].pop(listen_id)
+        with open("data.pkl", "wb") as wf:
+            pickle.dump(data_global, wf)
 
 
 if __name__ == '__main__':
---
+++ PyWeChatSpy/spy.py
@@ -0,0 +1,169 @@
+from ast import literal_eval
+import json
+import os
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+from time import sleep
+from subprocess import Popen, PIPE
+from .exceptions import handle_error_code
+import logging
+import warnings
+import re
+
+pattern = '[\u4e00-\u9fa5]'
+formatter = logging.Formatter('%(asctime)s [%(threadName)s] %(levelname)s: %(message)s')
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+sh.setLevel(logging.DEBUG)
+
+
+class WeChatSpy:
+    def __init__(self, parser=None, error_handle=None, download_image=False, multi=False):
+        self.logger = logging.getLogger()
+        self.logger.addHandler(sh)
+        self.logger.setLevel(logging.DEBUG)
+        # 是否下载图片(小于2MB)
+        # 该参数为True时 微信会自动下载收到的小于2MB的图片 但会造成图片消息延迟响应
+        self.__download_image = download_image
+        # TODO: 异常处理函数
+        self.__error_handle = error_handle
+        # 是否多开微信PC客户端
+        self.__multi = multi
+        # socket数据处理函数
+        self.__parser = parser
+        self.__pid2client = {}
+        self.__socket_server = socket(AF_INET, SOCK_STREAM)
+        self.__socket_server.bind(("127.0.0.1", 9527))
+        self.__socket_server.listen(1)
+        t_start_server = Thread(target=self.__start_server)
+        t_start_server.daemon = True
+        t_start_server.name = "socket accept"
+        t_start_server.start()
+
+    def add_log_output_file(self, filename="spy.log", mode='a', encoding="utf8", delay=False, level="WARNING"):
+        fh = logging.FileHandler(filename, mode=mode, encoding=encoding, delay=delay)
+        if level.upper() == "DEBUG":
+            fh.setLevel(logging.DEBUG)
+        elif level.upper() == "INFO":
+            fh.setLevel(logging.INFO)
+        elif level.upper() == "WARNING":
+            fh.setLevel(logging.WARNING)
+        elif level.upper() == "ERROR":
+            fh.setLevel(logging.ERROR)
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+    def __start_server(self):
+        while True:
+            socket_client, client_address = self.__socket_server.accept()
+            self.logger.info(f"A WeChat process from {client_address} successfully connected")
+            if self.__download_image:
+                self.__send({"code": 1}, client_address[1])
+            t_socket_client_receive = Thread(target=self.receive, args=(socket_client, ))
+            t_socket_client_receive.name = f"client {client_address[1]}"
+            t_socket_client_receive.daemon = True
+            t_socket_client_receive.start()
+
+    def receive(self, socket_client):
+        data_str = ""
+        _data_str = None
+        while True:
+            try:
+                _data_str = socket_client.recv(4096).decode(encoding="utf8", errors="ignore")
+            except Exception as e:
+                return self.logger.error(e)
+            if _data_str:
+                data_str += _data_str
+            if data_str and data_str.endswith("*393545857*"):
+                for data in data_str.split("*393545857*"):
+                    if data:
+                        data = literal_eval(data)
+                        if data["type"] == 1:
+                            self.__pid2client[data["pid"]] = socket_client
+                        if callable(self.__parser):
+                            self.__parser(data)
+                data_str = ""
+
+    def __send(self, data, pid):
+        if pid:
+            socket_client = self.__pid2client.get(pid)
+        else:
+            socket_client_list = list(self.__pid2client.values())
+            socket_client = socket_client_list[0] if socket_client_list else None
+        if socket_client:
+            data = json.dumps(data)
+            data_length_bytes = int.to_bytes(len(data.encode(encoding="utf8")), length=4, byteorder="little")
+            socket_client.send(data_length_bytes + data.encode(encoding="utf8"))
+
+    def run(self, background=False):
+        current_path = os.path.split(os.path.abspath(__file__))[0]
+        launcher_path = os.path.join(current_path, "Launcher.exe")
+        cmd_str = f"{launcher_path} multi" if self.__multi else launcher_path
+        p = Popen(cmd_str, shell=True, stdout=PIPE)
+        res_code, err = p.communicate()
+        res_code = res_code.decode()
+        handle_error_code(res_code)
+        if not background:
+            while True:
+                sleep(86400)
+        return int(res_code)
+
+    def query_contact_details(self, wxid, chatroom_wxid="", pid=None):
+        """
+        查询联系人详情
+        :param wxid: 联系人wxid
+        :param chatroom_wxid:
+        :param pid:
+        """
+        data = {"code": 2, "wxid": wxid, "chatroom_wxid": chatroom_wxid}
+        self.__send(data, pid)
+
+    def send_text(self, wxid, content, at_wxid="", pid=None):
+        """
+        发送文本消息
+        :param wxid: 文本消息接收wxid
+        :param content: 文本消息内容
+        :param at_wxid: 如果wxid为群wxid且需要@群成员 此参数为被@群成员wxid，以英文逗号分隔
+        :param pid:
+        """
+        if not wxid.endswith("chatroom"):
+            at_wxid = ""
+        data = {"code": 5, "wxid": wxid, "at_wxid": at_wxid, "content": content}
+        self.__send(data, pid)
+
+    def send_image(self, wxid, image_path, pid=None):
+        warnings.warn("The 'send_image' kwarg is deprecated, and has been replaced by the 'send_file'",
+                      DeprecationWarning)
+        self.send_file(wxid, image_path, pid)
+
+    def send_file(self, wxid, file_path, pid=None):
+        """
+        发送文件消息
+        :param wxid: 文件消息接收wxid
+        :param file_path: 文件路径
+        :param pid:
+        """
+        if len(file_path.split("\\")) > 8:
+            return self.logger.warning(f"File path is too long: {file_path}")
+        if re.findall(pattern, file_path):
+            return self.logger.warning(f"Chinese characters are not allowed in file path: {file_path}")
+        data = {"code": 6, "wxid": wxid, "file_path": file_path}
+        self.__send(data, pid)
+
+    def query_contact_list(self, step=50, pid=None):
+        """
+        查询联系人详情
+        :param step: 每次回调的联系人列表长度
+        :param pid:
+        :return:
+        """
+        data = {"code": 3, "key": "baofengxueonly", "step": step}
+        self.__send(data, pid)
+
+    def accept_new_contact(self, encryptusername, ticket, pid=None):
+        data = {"code": 7, "key": "baofengxueonly", "encryptusername": encryptusername, "ticket": ticket}
+        self.__send(data, pid)
+
+    def query_chatroom_member(self, wxid, pid=None):
+        data = {"code": 4, "wxid": wxid, "key": "baofengxueonly"}
+        self.__send(data, pid)
